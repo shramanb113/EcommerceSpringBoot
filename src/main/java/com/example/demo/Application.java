5ec306cb0cfb263d2f8ec0c271357a65193e4966
shramanb113 <shramanb1@gmail.com>
2025-07-03 00:43:28 +0530

    Added .env setup usinf springboot-dotenv and dotenv-java

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvEntry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach(entry -> {
+			System.setProperty(entry.getKey(), entry.getValue());
+		});
 		SpringApplication.run(Application.class, args);
 	}
 
